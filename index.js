ff033f7d1cd4514b3f6ff106fc455a4f01477e6d
Carlos <garcia.edu21@hotmail.com>
2021-03-25 20:35:52 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+//move left
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === 37) {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+
+  // move right
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === 37) {
+      moveDodgerLeft();
+    }
+  });
+
